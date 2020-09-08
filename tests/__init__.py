766af1307d91b57c399610ea7e7c77d78924f73b
Adrián Bernardi <adrian.bernardi@gmail.com>
2020-09-08 17:00:30 -0300

    Update tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,9 @@
-# The COPYRIGHT file at the top level of this repository contains the
-# full copyright notices and license terms.
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
 
 try:
-    from trytond.modules.current_account.tests.current_account import suite
+    from trytond.modules.current_account.tests.test_current_account \
+        import suite
 except ImportError:
     from .test_current_account import suite
 
