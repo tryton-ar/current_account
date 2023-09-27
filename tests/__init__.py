2c47296d0c7b689914be3d040799780fb9931c07
Adrián Bernardi <adrian.bernardi@gmail.com>
2023-09-26 23:08:23 -0300

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,2 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-
-try:
-    from trytond.modules.current_account.tests.test_current_account \
-        import suite
-except ImportError:
-    from .test_current_account import suite
-
-__all__ = ['suite']
